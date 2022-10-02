4e0c5628b7a14e611b047a6a1aa51ecb2b82bb7e
gakhuta <gakhutishvili29@gmail.com>
2022-10-02 16:24:07 +0400

    added changes

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ let slider = document.querySelector('.slider')
 menuBtn.addEventListener ('click', () => {
     slider.classList.toggle('active')
 })
+
+if (localStorage.getItem('LS_values')) {
+    let values = JSON.parse(localStorage.getItem('LS_values'))
+    document.querySelector('.register-btn').textContent = `Welcome back ${values.usr}!` 
+}
